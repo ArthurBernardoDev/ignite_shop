7f1ca0e861d2da5b987f3b753d771a2d25c756d1
Arthur Bernardo <arthurutschgg@gmail.com>
2022-10-25 15:02:17 -0300

    feat: add header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { globalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
+globalStyles();
 
 function MyApp({ Component, pageProps }) {
-  globalStyles();
-  
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={'./logo.svg'} />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
 
 export default MyApp;
